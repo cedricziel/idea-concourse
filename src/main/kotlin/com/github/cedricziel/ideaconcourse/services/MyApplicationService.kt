59727be0f37067fa97dd8184caeb699312cf2b45
GitHub Action <action@github.com>
2020-12-10 10:45:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/cedricziel/ideaconcourse/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cedricziel/ideaconcourse/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cedricziel.ideaconcourse.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cedricziel.ideaconcourse.MyBundle
 
 class MyApplicationService {
 

88b928fac251971fddd7ddf4409bf53499449f28
Cedric Ziel <cedric@cedric-ziel.com>
2020-12-12 15:32:45 +0100

    Rename bundle

--- src/main/kotlin/com/cedricziel/idea/concourse/ConcourseBundle.kt
+++ src/main/kotlin/com/cedricziel/idea/concourse/ConcourseBundle.kt
@@ -5,10 +5,9 @@ import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.ConcourseBundle"
 
-object MyBundle : AbstractBundle(BUNDLE) {
-
+object ConcourseBundle : AbstractBundle(BUNDLE) {
     @Suppress("SpreadOperator")
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) = getMessage(key, *params)

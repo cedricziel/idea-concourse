48fa3fbd84d1604a3ee4292942bc4982916b146d
Cedric Ziel <cedric@cedric-ziel.com>
2020-12-13 18:22:58 +0100

    Add Input and Output icons

--- src/main/kotlin/com/cedricziel/idea/concourse/ConcourseIcons.kt
+++ src/main/kotlin/com/cedricziel/idea/concourse/ConcourseIcons.kt
@@ -3,6 +3,8 @@ package com.cedricziel.idea.concourse
 import com.intellij.icons.AllIcons
 
 object ConcourseIcons {
+    val INPUT = AllIcons.Graph.ZoomIn
+    val OUTPUT = AllIcons.Graph.ZoomOut
     val RESOURCE = AllIcons.Ide.Notification.Gear
     val RESOURCE_TYPE = AllIcons.Ide.Gift
     val YAML = AllIcons.FileTypes.Yaml

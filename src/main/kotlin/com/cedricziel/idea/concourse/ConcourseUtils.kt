ceffece53319938fa07fcb425be785a88f79b55a
Cedric Ziel <cedric@cedric-ziel.com>
2020-12-13 09:41:11 +0100

    Convert ConcourseUtils to object since it shouldnt hold state

--- src/main/kotlin/com/cedricziel/idea/concourse/ConcourseUtils.kt
+++ src/main/kotlin/com/cedricziel/idea/concourse/ConcourseUtils.kt
@@ -3,25 +3,23 @@ package com.cedricziel.idea.concourse
 import com.cedricziel.idea.concourse.psi.visitor.ResourceNamesYamlVisitor
 import com.intellij.psi.PsiFile
 
-class ConcourseUtils {
-    companion object {
-        fun resourceSteps(): List<String> {
-            return listOf("get", "put")
-        }
+object ConcourseUtils {
+    fun resourceSteps(): List<String> {
+        return listOf("get", "put")
+    }
 
-        fun findResourcesInFile(containingFile: PsiFile?): List<String> {
-            if (containingFile == null) {
-                return arrayListOf()
-            }
+    fun findResourcesInFile(containingFile: PsiFile?): List<String> {
+        if (containingFile == null) {
+            return arrayListOf()
+        }
 
         val visitor = ResourceNamesYamlVisitor()
         visitor.visitFile(containingFile)
 
-            return visitor.resources.keys.toList()
-        }
+        return visitor.resources.keys.toList()
+    }
 
-        fun isPipelineFile(file: PsiFile): Boolean {
-            return file.name == "pipeline.yml"
-        }
+    fun isPipelineFile(file: PsiFile): Boolean {
+        return file.name == "pipeline.yml"
     }
 }
